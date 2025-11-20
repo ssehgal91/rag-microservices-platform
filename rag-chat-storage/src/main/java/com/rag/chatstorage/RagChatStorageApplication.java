e5228c85588efaa6b136a201673564f9dbf5865f
Sehgal Sakshi, (Sakshi.Sehgal@partner.bmwgroup.com) <Sakshi.Sehgal+bmw@partner.bmwgroup.com>
2025-11-20 18:43:06 +0530

    Updated for config and discovery server

--- rag-chat-storage/src/main/java/com/rag/chatstorage/RagChatStorageApplication.java
+++ rag-chat-storage/src/main/java/com/rag/chatstorage/RagChatStorageApplication.java
@@ -8,7 +8,6 @@ public class RagChatStorageApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("chat storage called");
 		SpringApplication.run(RagChatStorageApplication.class, args);
 	}
 
